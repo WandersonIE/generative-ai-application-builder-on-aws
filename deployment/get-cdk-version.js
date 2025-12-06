14bdd2640cc903b0eaad465583324edd3e458f5e
MAGO TIÇÃO <socrates070@gmail.com>
2025-12-06 18:11:08 -0400

    Delete deployment directory

--- deployment/get-cdk-version.js
+++
@@ -1,5 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
-const pkg = require("../source/infrastructure/node_modules/aws-cdk/package.json");
-console.log(pkg.version);
